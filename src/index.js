35a176f4d728f6a0835ad032d14eec7d05d0c8e8
Afnan <rikzaafnan@gmail.com>
2020-07-13 22:46:38 +0700

    add header

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter } from "react-router-dom"
+
 ReactDOM.render(
-  <React.StrictMode>
+
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
+
+  // <React.StrictMode>
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
